d109be5851b54b113f7d2e86e61ba31a9845b8b0
a.milinevskiy@solarsecurity.ru <a.milinevskiy@rt-solar.ru>
2019-10-28 17:12:39 +0300

    add Contact class

--- add_contact.py
+++ add_contact.py
@@ -6,29 +6,34 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+from contact import Contact
 
 class AddContact(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-    
-    def test_add_contact(self):
-        wd = self.wd
-        wd.get("http://10.201.48.35/addressbookv4.1.4/group.php")
-        wd.find_element_by_link_text("home").click()
+
+    def create_new_contact(self, wd, contact):
+        # creation initialization
         wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").send_keys("User_1")
-        wd.find_element_by_name("lastname").send_keys("User_11")
-        wd.find_element_by_name("address").send_keys("Moscow, Test street")
-        wd.find_element_by_name("home").send_keys("+79296365878")
-        wd.find_element_by_name("mobile").send_keys("+78963210011")
-        wd.find_element_by_name("email").send_keys("user_1.user_11@gmail.com")
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("3")
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("April")
-        wd.find_element_by_name("byear").send_keys("1985")
+        # form fulfilment
+        wd.find_element_by_name("firstname").send_keys(contact.f_name)
+        wd.find_element_by_name("lastname").send_keys(contact.l_name)
+        wd.find_element_by_name("address").send_keys(contact.addr)
+        wd.find_element_by_name("home").send_keys(contact.h_phone)
+        wd.find_element_by_name("mobile").send_keys(contact.m_phone)
+        wd.find_element_by_name("email").send_keys(contact.e_mail)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.day_dob)
+        # wd.find_element_by_xpath("//div[@id='content']/form/select/option[8]").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.month_dob)
+        # wd.find_element_by_xpath("//div[@id='content']/form/select[2]/option[5]").click()
+        wd.find_element_by_name("byear").send_keys(contact.year_dob)
+        # submitting form
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("home").click()
-    
+
+    def open_home_page(self, wd):
+        wd.get("http://10.201.48.35/addressbookv4.1.4/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
@@ -41,6 +46,12 @@ class AddContact(unittest.TestCase):
     
     def tearDown(self):
         self.wd.quit()
+
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.create_new_contact(wd, Contact(f_name="First_Name", l_name="Last_Name", m_phone="+7888996755", day_dob="15", month_dob="May", year_dob="1987"))
+        self.open_home_page(wd)
 
 
 if __name__ == "__main__":
---
+++ contact.py
@@ -0,0 +1,11 @@
+class Contact:
+    def __init__(self, f_name, l_name, addr="Default City", h_phone="+79295341678", m_phone="+78856781256", e_mail="User_test@gh.ty", day_dob="5", month_dob="August", year_dob="1986"):
+        self.f_name = f_name
+        self.l_name = l_name
+        self.addr = addr
+        self.h_phone = h_phone
+        self.m_phone = m_phone
+        self.e_mail = e_mail
+        self.day_dob = day_dob
+        self.month_dob = month_dob
+        self.year_dob = year_dob
