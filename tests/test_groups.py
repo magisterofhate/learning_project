2aafe2c8d89cf6ca004ae9cf2bbbaa2b02eb5580
a.milinevskiy@solarsecurity.ru <a.milinevskiy@rt-solar.ru>
2019-10-29 18:43:47 +0300

    uncomment tests for group add and delete

--- tests/test_groups.py
+++ tests/test_groups.py
@@ -2,18 +2,18 @@
 from model.group import Group
 
 
-# def test_add_group(app):
-#     app.navigation.home_page()
-#     app.navigation.group_list()
-#     app.create_items.create_group(Group("name777", "header78", "footer_test"))
-#     app.navigation.group_list()
-#
-#
-# def test_del_group(app):
-#     app.navigation.home_page()
-#     app.navigation.group_list()
-#     app.delete_items.delete_group()
-#     app.navigation.group_list()
+def test_add_group(app):
+    app.navigation.home_page()
+    app.navigation.group_list()
+    app.create_items.create_group(Group("name777", "header78", "footer_test"))
+    app.navigation.group_list()
+
+
+def test_del_group(app):
+    app.navigation.home_page()
+    app.navigation.group_list()
+    app.delete_items.delete_group()
+    app.navigation.group_list()
 
 
 def test_mod_group(app):
