0fc34c0fa7759d5c0fa8f6af1390bb8dbaad1831
a.milinevskiy@solarsecurity.ru <a.milinevskiy@rt-solar.ru>
2019-10-31 16:12:27 +0300

    uncommented contact tests

--- tests/test_contacts.py
+++ tests/test_contacts.py
@@ -2,12 +2,12 @@
 from model.contact import Contact
 
 
-# def test_add_contact(app):
-#     app.navigation.home_page()
-#     app.navigation.login()
-#     app.create_items.create_contact(Contact(f_name="First_Name", l_name="Last_Name", m_phone="+7888996755", day_dob="22", month_dob="September", year_dob="1987"))
-#     app.navigation.home_page()
-#     app.navigation.logout()
+def test_add_contact(app):
+    app.navigation.home_page()
+    app.navigation.login()
+    app.create_items.create_contact(Contact(f_name="First_Name", l_name="Last_Name", m_phone="+7888996755", day_dob="22", month_dob="September", year_dob="1987"))
+    app.navigation.home_page()
+    app.navigation.logout()
 
 
 def test_modify_contact(app):
@@ -26,20 +26,20 @@ def test_modify_contact(app):
     app.navigation.logout()
 
 
-# def test_delete_contact_from_edit_form(app):
-#     app.navigation.home_page()
-#     app.navigation.login()
-#     app.helpers.choose_rnd_user()
-#     app.delete_items.delete_contact()
-#     app.navigation.home_page()
-#     app.navigation.logout()
-#
-#
-# def test_delete_contact_from_the_list(app):
-#     app.navigation.home_page()
-#     app.navigation.login()
-#     app.helpers.choose_rnd_el()
-#     app.delete_items.delete_contact()
-#     app.helpers.confirm_on_popup()
-#     app.navigation.logout()
+def test_delete_contact_from_edit_form(app):
+    app.navigation.home_page()
+    app.navigation.login()
+    app.helpers.choose_rnd_user()
+    app.delete_items.delete_contact()
+    app.navigation.home_page()
+    app.navigation.logout()
+
+
+def test_delete_contact_from_the_list(app):
+    app.navigation.home_page()
+    app.navigation.login()
+    app.helpers.choose_rnd_el()
+    app.delete_items.delete_contact()
+    app.helpers.confirm_on_popup()
+    app.navigation.logout()
 
