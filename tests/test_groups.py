6f696ef06a9fa40ff4b6d90290a445e26bfab36a
a.milinevskiy@solarsecurity.ru <a.milinevskiy@rt-solar.ru>
2019-11-01 13:06:44 +0300

    added pre-conditions to modify, delete group tests

--- tests/test_groups.py
+++ tests/test_groups.py
@@ -2,17 +2,20 @@
 from model.group import Group
 
 
-# def test_add_group(app):
-#     app.navigation.group_list()
-#     app.create_items.create_group(Group("name777", "header78", "footer_test"))
-#     app.navigation.group_list()
-#
-#
-# def test_del_group(app):
-#     app.navigation.group_list()
-#     app.helpers.choose_rnd_el()
-#     app.delete_items.delete_group()
-#     app.navigation.group_list()
+def test_add_group(app):
+    app.navigation.group_list()
+    app.create_items.create_group(Group("name777", "header78", "footer_test"))
+    app.navigation.group_list()
+
+
+def test_del_group(app):
+    app.navigation.group_list()
+    if app.helpers.check_elements_presented() is False:
+        app.create_items.create_group(Group("name777", "header78", "footer_test"))
+    app.navigation.group_list()
+    app.helpers.choose_rnd_el()
+    app.delete_items.delete_group()
+    app.navigation.group_list()
 
 
 def test_mod_group(app):
