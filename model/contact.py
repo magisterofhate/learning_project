89290c0e0726711a5f2036f76257205dcd10bc5c
a.milinevskiy@solarsecurity.ru <a.milinevskiy@rt-solar.ru>
2019-11-08 13:13:14 +0300

    added id to group and contact models

--- model/contact.py
+++ model/contact.py
@@ -1,5 +1,7 @@
 class Contact:
-    def __init__(self, f_name, l_name, addr="Default City", h_phone="+79295341678", m_phone="+78856781256", e_mail="User_test@gh.ty", day_dob="5", month_dob="August", year_dob="1986"):
+    def __init__(self, id=None, f_name='Firstname', l_name='Lastname', addr="Default City", h_phone="+79295341678",
+                 m_phone="+78856781256", e_mail="User_test@gh.ty", day_dob="5", month_dob="August", year_dob="1986"):
+        self.id = id
         self.f_name = f_name
         self.l_name = l_name
         self.addr = addr
--- model/group.py
+++ model/group.py
@@ -1,6 +1,7 @@
 class Group:
 
-    def __init__(self, name, header, footer):
+    def __init__(self, id=None, name='Name', header='Header', footer='Footer'):
+        self.id = id
         self.name = name
         self.header = header
         self.footer = footer
