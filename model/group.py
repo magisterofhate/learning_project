a931b3886967d9c6876485dfbedf4542f2c59ff6
a.milinevskiy@solarsecurity.ru <a.milinevskiy@rt-solar.ru>
2019-11-11 12:41:48 +0300

    redefined __eq__, __repr__, __lt__ methods for Groups

--- model/group.py
+++ model/group.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+
+
 class Group:
 
     def __init__(self, id=None, name='Name', header='Header', footer='Footer'):
@@ -5,3 +8,13 @@ class Group:
         self.name = name
         self.header = header
         self.footer = footer
+
+    def __repr__(self):
+        return "%s; %s" % (self.id, self.name)
+
+    def __lt__(self, other):
+        return self.id < other.id
+
+    def __eq__(self, other):
+        return (self.id == other.id or self.id == maxsize or other.id == maxsize) \
+               and self.name == other.name
