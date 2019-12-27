f6e0ecdb540433059da5b616c27ebdb74ec246f3
a.milinevskiy@solarsecurity.ru <a.milinevskiy@rt-solar.ru>
2019-12-27 12:05:47 +0300

    groups generator changed

--- generator/groups.py
+++ generator/groups.py
@@ -1,4 +1,4 @@
-import model.group as group
+from model.group import Group
 import fixture.common as common
 from sys import maxsize
 import os.path as op
@@ -26,7 +26,7 @@ def generate_group_data():
     new_head = common.clear_data(common.rnd_string(40))
     new_foot = common.clear_data(common.rnd_string(30))
     new_id = maxsize
-    return group.Group(id=new_id, name=new_name, header=new_head, footer=new_foot)
+    return Group(id=new_id, name=new_name, header=new_head, footer=new_foot)
 
 
 test_data = [
@@ -34,7 +34,7 @@ test_data = [
     for i in range(n)
 ]
 
-file = op.join(op.dirname(op.abspath(__file__)), "..", f)
+file = op.join(op.dirname(op.abspath(__file__)), f)
 
 with open(file, "w") as out:
     jsonpickle.set_encoder_options("json", indent=2)
--- tests/test_groups.py
+++ tests/test_groups.py
@@ -4,20 +4,20 @@ from fixture.group_operations import GroupOps
 from fixture.common import clean_db_groups
 
 
-def test_add_group(app, db, check_ui, test_data_groups):
-    group = test_data_groups
-    go = GroupOps(app)
-    app.navigation.group_list()
-    old_group_list = db.get_group_list_from_db()
-    test_group = group
-    go.create_group(test_group)
-    app.navigation.group_list()
-    old_group_list.append(test_group)
-    new_group_list = db.get_group_list_from_db()
-    assert sorted(new_group_list) == sorted(old_group_list)
-    if check_ui:
-        group_list_ui = go.get_group_list()
-        assert sorted(map(clean_db_groups, new_group_list)) == sorted(group_list_ui)
+# def test_add_group(app, db, check_ui, test_data_groups):
+#     group = test_data_groups
+#     go = GroupOps(app)
+#     app.navigation.group_list()
+#     old_group_list = db.get_group_list_from_db()
+#     test_group = group
+#     go.create_group(test_group)
+#     app.navigation.group_list()
+#     old_group_list.append(test_group)
+#     new_group_list = db.get_group_list_from_db()
+#     assert sorted(new_group_list) == sorted(old_group_list)
+#     if check_ui:
+#         group_list_ui = go.get_group_list()
+#         assert sorted(map(clean_db_groups, new_group_list)) == sorted(group_list_ui)
 
 
 def test_del_group(app, db, check_ui):
@@ -38,17 +38,17 @@ def test_del_group(app, db, check_ui):
         assert sorted(map(clean_db_groups, new_group_list)) == sorted(group_list_ui)
 
 
-def test_mod_group(app, db, check_ui):
-    go = GroupOps(app)
-    app.navigation.group_list()
-    if not go.groups_presented():
-        go.create_group(generate_group())
-    app.navigation.group_list()
-    new_group = generate_group(go.choose_rnd_group())
-    go.modify_group(new_group)
-    app.navigation.group_list()
-    new_group_list = db.get_group_list_from_db()
-    assert new_group in new_group_list
-    if check_ui:
-        group_list_ui = go.get_group_list()
-        assert sorted(map(clean_db_groups, new_group_list)) == sorted(group_list_ui)
+# def test_mod_group(app, db, check_ui):
+#     go = GroupOps(app)
+#     app.navigation.group_list()
+#     if not go.groups_presented():
+#         go.create_group(generate_group())
+#     app.navigation.group_list()
+#     new_group = generate_group(go.choose_rnd_group())
+#     go.modify_group(new_group)
+#     app.navigation.group_list()
+#     new_group_list = db.get_group_list_from_db()
+#     assert new_group in new_group_list
+#     if check_ui:
+#         group_list_ui = go.get_group_list()
+#         assert sorted(map(clean_db_groups, new_group_list)) == sorted(group_list_ui)
